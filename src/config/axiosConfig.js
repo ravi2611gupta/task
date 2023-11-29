6338f17c6e0fdbd72ad12ed83f19b7d87f1cda4b
Ravi Gupta <ravi2611gupta@gmail.com>
2023-11-30 04:04:54 +0530

    🐻‍ added server URL

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // http://localhost:5000/api
 // https://task-backend-lime.vercel.app/api
 const myAxios = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-backend-lime.vercel.app/api',
 });
 
 export default myAxios;
